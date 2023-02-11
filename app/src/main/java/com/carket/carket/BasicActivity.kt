05543480b1cd0c92e7b9f4ef06f82b2e8be47dfb
ma7moud3zim <eng3zim@gmail.com>
2023-02-11 15:04:31 +0200

    Add BaseActivity and Chooser, Application Runs correctly but there are issues with FB

---
+++ app/src/main/java/com/carket/carket/BasicActivity.kt
@@ -0,0 +1,13 @@
+package com.carket.carket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+open class BasicActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/carket/carket/ChooserActivity.kt
@@ -0,0 +1,13 @@
+package com.carket.carket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooserActivity : BasicActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chooser)
+    }
+
+}
