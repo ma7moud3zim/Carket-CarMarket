ee8fb0451e3fa222971a1ab4d2b908b6f93f0a40
root <root@pop-os.localdomain>
2023-02-11 20:45:52 +0200

    Adding Splash Activity

---
+++ app/src/main/java/com/carket/carket/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.carket.carket
+
+import android.content.Intent
+import android.os.Bundle
+
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Check if the user logged in
+        // True: fetch data and show it in home activity
+        // False: redirect to chooser activity
+
+        val intent = Intent(this@SplashActivity, ChooserActivity::class.java)
+        startActivity(intent)
+    }
+
+}
