f1e8f2e7c177ab0d2b65c8a7f908883214fbde70
ma7moud3zim <eng3zim@gmail.com>
2023-02-12 18:07:58 +0200

    Design CustomerLoginActivity
    I make a simple Design for the login screen of customer and put an event when you click the button in the chooser screen so it redirect you to the screen of log in.

--- app/src/main/java/com/carket/carket/ChooserActivity.kt
+++ app/src/main/java/com/carket/carket/ChooserActivity.kt
@@ -1,12 +1,23 @@
 package com.carket.carket
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class ChooserActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chooser)
+
+        val goToCus = findViewById<Button>(R.id.goToCus)
+
+        goToCus.setOnClickListener {
+            val intent = Intent(this@ChooserActivity, CustomerLogin::class.java)
+            startActivity(intent)
+        }
     }
+
+
 
 }
---
+++ app/src/main/java/com/carket/carket/CustomerLogin.kt
@@ -0,0 +1,11 @@
+package com.carket.carket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_login)
+    }
+}
