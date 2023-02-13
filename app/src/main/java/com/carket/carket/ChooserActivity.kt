caacf22af037a893c26cb437e058fbde8bef264e
ma7moud3zim <eng3zim@gmail.com>
2023-02-13 21:27:11 +0200

    Handle Chooser
    Handled the buttons in chooser activity and also added publisher activity to app manifest

--- app/src/main/java/com/carket/carket/ChooserActivity.kt
+++ app/src/main/java/com/carket/carket/ChooserActivity.kt
@@ -10,12 +10,21 @@ class ChooserActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chooser)
 
+        // Handling the Button of CustomerLogin
         val goToCus = findViewById<Button>(R.id.goToCus)
-
         goToCus.setOnClickListener {
             val intent = Intent(this@ChooserActivity, CustomerLogin::class.java)
             startActivity(intent)
         }
+
+
+        // Handling the Button of PublisherLogin
+        val goToPub = findViewById<Button>(R.id.goToPub)
+        goToPub.setOnClickListener {
+            val intent = Intent(this@ChooserActivity, PublisherLogin::class.java)
+            startActivity(intent)
+        }
+
     }
 
 
