76d783942da16154ce0751d64aa20b4eb5c672ad
ma7moud3zim <eng3zim@gmail.com>
2023-02-15 17:58:15 +0200

    Fix issue
    Fixed the issue of making two icons for running and that by removing intent filter for chooserActivity and left just one for Splash-Screen.

---
+++ app/src/main/java/com/carket/carket/SignCustomer.kt
@@ -0,0 +1,11 @@
+package com.carket.carket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignCustomer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_customer)
+    }
+}
