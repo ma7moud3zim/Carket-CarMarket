b1e43d3fa6807e884bbaf0f0bf64e1b1560052ef
ma7moud3zim <eng3zim@gmail.com>
2023-02-15 19:52:40 +0200

    Desing of SignUp customer

--- app/src/main/java/com/carket/carket/CustomerLogin.kt
+++ app/src/main/java/com/carket/carket/CustomerLogin.kt
@@ -1,11 +1,23 @@
 package com.carket.carket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class CustomerLogin : AppCompatActivity() {
+class CustomerLogin : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_login)
+
+
+        // Go to SignUp a new Customer
+        val goToCusSign = findViewById<Button>(R.id.goToCusSign)
+        goToCusSign.setOnClickListener {
+            val intent = Intent(this@CustomerLogin, SignCustomer::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
