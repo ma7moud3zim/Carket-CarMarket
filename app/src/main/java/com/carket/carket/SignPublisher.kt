825859e59f639924164959f3a69b760c3cef5166
codingtolife <sharmilakorada23@gmail.com>
2023-02-17 00:05:20 +0530

    Changed the signup button colour

--- app/src/main/java/com/carket/carket/SignPublisher.kt
+++ app/src/main/java/com/carket/carket/SignPublisher.kt
@@ -1,4 +1,11 @@
 package com.carket.carket
 
-class SignPublisher {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignPublisher : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_publisher)
+    }
 }
