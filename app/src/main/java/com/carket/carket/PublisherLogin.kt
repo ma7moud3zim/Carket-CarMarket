4a887cd81ad737cf9cc8e6ef1351ee32e05e5978
ma7moud3zim <eng3zim@gmail.com>
2023-02-17 20:02:18 +0200

    Handling button
    Just handling the button of Sign up in publisher Login activity.

--- app/src/main/java/com/carket/carket/PublisherLogin.kt
+++ app/src/main/java/com/carket/carket/PublisherLogin.kt
@@ -1,11 +1,22 @@
 package com.carket.carket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PublisherLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_publisher_login)
+
+
+        // Go to SignUp a new Publisher
+        val goToCusSign = findViewById<Button>(R.id.goToPubSign)
+        goToCusSign.setOnClickListener {
+            val intent = Intent(this@PublisherLogin, SignPublisher::class.java)
+            startActivity(intent)
+        }
+
     }
 }
