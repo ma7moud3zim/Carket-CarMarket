1f63e6ed9259bca55a268e455c193b19b618277d
ma7moud3zim <eng3zim@gmail.com>
2023-02-20 04:09:05 +0200

    Add customer DataClass
    Add the dependency of firebase and construct the class of Customer and insert a user for the database to insure it is working.

---
+++ app/src/main/java/com/carket/carket/Customer.kt
@@ -0,0 +1,5 @@
+package com.carket.carket
+
+data class Customer(val name:String? =null , val usernm:String? =null ,val email:String? =null
+                    ,val pass:String? =null ,val phn:String? =null ,val loc:String? =null)
+
--- app/src/main/java/com/carket/carket/SignCustomer.kt
+++ app/src/main/java/com/carket/carket/SignCustomer.kt
@@ -1,11 +1,45 @@
 package com.carket.carket
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignCustomer : BaseActivity() {
+    // The initialize of databaseReference important for inserting data
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_customer)
+        val signBtn = findViewById<Button>(R.id.SignBtn)
+
+
+        // The Button of signing up a new user and inserting it to the firebase
+        signBtn.setOnClickListener{
+            val name = findViewById<EditText>(R.id.nameCustSign).text.toString()
+            val usernm = findViewById<EditText>(R.id.usrnmCustSign).text.toString().trim()
+            val mail = findViewById<EditText>(R.id.mailCustSign).text.toString()
+            val pass = findViewById<EditText>(R.id.passCustSign).text.toString()
+            val phone = findViewById<EditText>(R.id.phoneCustSign).text.toString()
+            val loc = findViewById<EditText>(R.id.locCustSign).text.toString()
+
+            database = FirebaseDatabase.getInstance().getReference("Custs")
+            val cust = Customer(name,usernm,mail,pass,phone,loc)
+
+
+            database.child(usernm).setValue(cust).addOnSuccessListener {
+                Toast.makeText(this, "Successfully saved" , Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener{
+                Toast.makeText(this, "Faaailed" , Toast.LENGTH_SHORT).show()
+                }
+        }
+
     }
+
+
+
+
 }
