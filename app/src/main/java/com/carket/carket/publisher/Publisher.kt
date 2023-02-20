26e4b65e4e2033ece64ebee608e3f1390f087935
codingtolife <sharmilakorada23@gmail.com>
2023-02-20 23:01:46 +0530

    Added Publisher data class

---
+++ app/src/main/java/com/carket/carket/publisher/Publisher.kt
@@ -0,0 +1,4 @@
+package com.carket.carket.publisher
+
+data class Publisher(val name:String? =null , val usernm:String? =null ,val email:String? =null
+                     ,val pass:String? =null ,val phn:Number? =null ,val loc:String? =null)
