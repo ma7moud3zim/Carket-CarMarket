b1f4ff8e461756604b94ecfadd1095e1fc17ebc5
ma7moud3zim <eng3zim@gmail.com>
2023-03-05 03:52:38 +0200

    CreateHomeActivity
    Create a home activity for customer and added it to xml file

---
+++ app/src/main/java/com/carket/carket/customer/HomeActivityCust.kt
@@ -0,0 +1,13 @@
+package com.carket.carket.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.carket.carket.BaseActivity
+import com.carket.carket.R
+
+class HomeActivityCust : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_cust)
+    }
+}
