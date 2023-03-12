2ff39b72ad93627fa032ff5ea9dd8a3149503d26
ma7moud3zim <eng3zim@gmail.com>
2023-03-12 06:11:45 +0200

    Make Car DataClass
    Write a code for the car attributes and adding an element to the firebase to retireve it. Also learning how to upload an Image and retrieving it.

---
+++ app/src/main/java/com/carket/carket/Car.kt
@@ -0,0 +1,5 @@
+package com.carket.carket
+
+data class Car(val car_id:String? =null , val company:String? =null ,val country:String? =null
+               ,val model:String? =null ,val title:Number? =null ,val price:Int? =null
+               , val year:String?=null ,val imageid:String? = null )
