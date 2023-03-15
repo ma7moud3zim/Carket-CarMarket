5542f5249ec8e67becda0c79ef4e7222eeecbd30
codingtolife <sharmilakorada23@gmail.com>
2023-03-15 22:53:06 +0530

    Made changes in navigation menu for the publisher

--- app/src/main/java/com/carket/carket/publisher/HomePublisher.kt
+++ app/src/main/java/com/carket/carket/publisher/HomePublisher.kt
@@ -1,4 +1,47 @@
 package com.carket.carket.publisher
 
-class HomePublisher {
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.carket.carket.BaseActivity
+import com.carket.carket.R
+import com.google.android.material.navigation.NavigationView
+
+class HomePublisher:BaseActivity() {
+    lateinit var toggle : ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_pub)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.homepublisher)
+        val navView: NavigationView = findViewById(R.id.navigation)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.profilepub -> Toast.makeText(this, "profile clicked", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.favrpub -> Toast.makeText(this, "favourite clicked", Toast.LENGTH_SHORT).show()
+                R.id.setpub -> Toast.makeText(this, "favourite clicked", Toast.LENGTH_SHORT).show()
+                R.id.logoutpub -> Toast.makeText(this, "logout clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+            return true
+        return true
+    }
+
 }
